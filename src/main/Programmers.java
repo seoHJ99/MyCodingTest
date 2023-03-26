32d9dca34ad34081129eff1df57ac253606088ba
MakingNameIsAnnoying <9669579@naver.com>
2023-03-27 00:31:14 +0900

    실패
    원인. 연속된 숫자가 a,b,c가 들어올때, a와 c 사이의 거리가 m보다 작으면 1로 카운트 되어야 하는데 2로 카운트 됨.

--- src/main/Programmers.java
+++ src/main/Programmers.java
@@ -10,7 +10,7 @@ public class Programmers {
     // 칠하지 않기로 한 곳을 다시 칠해도 됨
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution(8,4, new int[]{2,3,6});
+        solution.solution(8,4, new int[]{1,2,3,5});
     }
 }
 
@@ -20,14 +20,20 @@ class Solution {
         int answer = 0;
         int now = 0;
         int next = 0;
-        for(int i =0; i<section.length-1; i++){
+        for(int i =0; i<section.length; i++){
             if(m >1) {
                 now = section[i];
+                if (i < section.length - 1) {
                     next = section[i + 1];
-                int distance = next - now + 1;
-                if (distance <= m) {
-                    answer += distance / m;
-                    if (distance % m > 0) {
+                }
+                int distance = next - now;
+                if (distance != 0) {
+                    if (distance <= m) {
+                        answer += distance / m;
+                        if (distance % m > 0 && now != next) {
+                            answer++;
+                        }
+                    } else {
                         answer++;
                     }
                 }
