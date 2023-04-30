66fc902a61c9ad5850365f13163765e357ee329b
MakingNameIsAnnoying <9669579@naver.com>
2023-04-30 23:51:12 +0900

    d
    d

--- 인프런 강의/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ 인프런 강의/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,9 +1,11 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,4 +29,12 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 20000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(2000);
     }
+
+//    @Test
+//    void fieldInjectionTest(){
+//        OrderServiceImpl orderService1 = new OrderServiceImpl();
+//        orderService1.setDiscountPolicy(new FixDiscountPolicy());
+//        orderService1.setMemberRepository(new MemoryMemberRepository());
+//        orderService1.createOrder(1L, "item1", 1000);
+//    }
 }
