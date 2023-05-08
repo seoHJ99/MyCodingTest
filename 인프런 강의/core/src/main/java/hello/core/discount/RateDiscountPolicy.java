72710b46fe4160eb184f69be34347f60400d66fc
MakingNameIsAnnoying <121778872+MakingNameIsAnnoying@users.noreply.github.com>
2023-05-08 18:17:29 +0900

    d
    d

--- 인프런 강의/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ 인프런 강의/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,4 +1,5 @@
 package hello.core.discount;
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -6,7 +7,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인
     @Override
