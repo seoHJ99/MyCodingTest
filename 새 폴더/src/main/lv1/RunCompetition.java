0dcf5bbe6591bf70200fea437c942d03b18e02c0
MakingNameIsAnnoying <9669579@naver.com>
2023-07-28 00:30:26 +0900

    map두개로 해결

--- 새 폴더/src/main/lv1/RunCompetition.java
+++ 새 폴더/src/main/lv1/RunCompetition.java
@@ -12,24 +12,38 @@ public class RunCompetition {
     }
 
     public String[] solution(String[] players, String[] callings) {
-        String[] answer;
-//        Map<Integer, String> orderMap = new HashMap<>();
-//        for(int i =0; i<players.length; i++){
-//            orderMap.put(i, players[i]);
-//        }
-        for(int i =0; i<callings.length; i++){
-            String outrun = callings[i];
-            for(int j =0; j<players.length; j++){
-                String name = players[j];
-                if(name.equals(outrun)){
-                    String outranMan = players[j-1];
-                    players[j-1] = outrun;
-                    players[j]=outranMan;
-                }
-            }
+        Map<String, Integer> nameMap = new HashMap<>();
+        Map<Integer, String> orderMap = new HashMap<>();
+
+        for(int i =0; i<players.length; i++){
+            nameMap.put(players[i], i);
+            orderMap.put(i,players[i]);
         }
-        answer = players;
-        return answer;
+        for(int i=0; i<callings.length; i++){
+            String callingMan = callings[i];
+            int order = nameMap.get(callingMan);
+            String outranMan = orderMap.get(order-1);
+            nameMap.put(callingMan, order-1);
+            nameMap.put(outranMan, order);
+            orderMap.put(order-1, callingMan);
+            orderMap.put(order, outranMan);
+        }
+        for(int i =0; i<players.length; i++){
+            players[i] = orderMap.get(i);
+        }
+
+//        for(int i =0; i<callings.length; i++){
+//            String outrun = callings[i];
+//            for(int j =0; j<players.length; j++){
+//                String name = players[j];
+//                if(name.equals(outrun)){
+//                    String outranMan = players[j-1];
+//                    players[j-1] = outrun;
+//                    players[j]=outranMan;
+//                }
+//            }
+//        }
+        return players;
     }
 
 }
