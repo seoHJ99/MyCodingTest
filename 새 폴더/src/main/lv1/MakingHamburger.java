1650f4a98cb8ab75c41a165ff830e8a81340c836
MakingNameIsAnnoying <9669579@naver.com>
2023-08-04 22:12:50 +0900

    다른 사람 풀이 보고 힌트 얻음
    배열을 리스트로 전환할때, java8이후로 지원하는 Arrays.boxed.toList 사용하니까 불변 객체가 돼서 요소 삭제가 불가능해짐.

--- 새 폴더/src/main/lv1/MakingHamburger.java
+++ 새 폴더/src/main/lv1/MakingHamburger.java
@@ -5,41 +5,33 @@ import java.util.List;
 
 public class MakingHamburger {
     public static void main(String[] args) {
-        int[] a = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        int[] a = {2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 3, 1, 3, 1};
         MakingHamburger makingHamburger = new MakingHamburger();
         makingHamburger.solution(a);
     }
     public int solution(int[] ingredient) {
         int answer = 0;
         List<Integer> list = new ArrayList<>();
-        for(int i =0; i<ingredient.length; i++){
-            list.add(ingredient[i]);
+        for (int i : ingredient) {
+            list.add(i);
         }
-        for(int i =0; i<list.size()-3; i++){
-            if(list.get(i) == 1 && list.get(i+1) == 2 && list.get(i+2) == 3 && list.get(i+3) == 1 ){
+        for(int i =0; i<list.size()-3;){
+            if(list.get(i)==1 && list.get(i+1) == 2 && list.get(i+2) == 3 && list.get(i+3) == 1){
                 answer++;
-                for(int j=0; j<4; j++){
-                    list.remove(i);
+                list.remove(i + 3);
+                list.remove(i + 2);
+                list.remove(i + 1);
+                list.remove(i);
+                if(i>3) {
+                    i -= 4;
+                }else {
+                    i = 0;
                 }
-                i=i-2;
+            }else {
+                i++;
             }
         }
         System.out.println(answer);
         return answer;
-//            String str="";
-//            for (int i=0; i<ingredient.length-2; i++){
-//                if(ingredient[i]==1 && ingredient[i+1]==2 && ingredient[i+2]==3){
-//                    for(int j=0; j<ingredient.length; j++) {
-//                        str += ingredient[j];
-//                    }
-//                }
-//            }
-//            int a = str.length();
-//            for(int i=0; i<ingredient.length; i++) {
-//                if (str.contains("1231")) {
-//                    str = str.replace("1231", "");
-//                    answer++;
-//                }
-//            }
     }
 }
