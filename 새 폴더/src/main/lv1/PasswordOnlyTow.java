12d7b695f3226eedbd783af508fa46b692e3bf19
MakingNameIsAnnoying <9669579@naver.com>
2023-08-18 23:25:30 +0900

    또 실패...

--- 새 폴더/src/main/lv1/PasswordOnlyTow.java
+++ 새 폴더/src/main/lv1/PasswordOnlyTow.java
@@ -1,39 +1,40 @@
 package main.lv1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PasswordOnlyTow {
     public static void main(String[] args) {
         PasswordOnlyTow pro = new PasswordOnlyTow();
-        pro.solution("aukks", "wbqd", 5);
+        pro.solution("a", "bcdefghijk", 20);
     }
 
     public String solution(String s, String skip, int index) {
         String answer = "";
-        int[] alphabet = new int[26];
-        for(int i =0; i<26; i++){
-            alphabet[i] = 'a'+i;
-        }
+        Set<Character> skipChar = new HashSet<>();
         for(int i =0; i<skip.length(); i++){
-            int start = skip.charAt(i);
-            for(int j = start; j<='z'; j++){
-                if(j + index >'z'){
-                    alphabet[j - 'a'] = alphabet[j-'a'] +index;
-                    alphabet[j - 'a' - 1] = alphabet[j-'a'];
-                }else {
-                    alphabet[j - 'a'] = j + index;
+            skipChar.add(skip.charAt(i));
+        }
+        for(int i=0; i<s.length(); i++){
+            char spell = s.charAt(i);
+            int moving = index;
+            for(int j =1; j<= moving;){
+                char a = (char) (spell +j);
+                if(skipChar.contains(a)){
+                    moving ++ ;
                 }
+                if(j == moving){
+                    spell += moving;
+                    if(spell > 'z'){
+                        spell = 'a';
+                        moving = moving -j;
+                        j = 0;
+                        continue;
+                    }
+                }
+                j++;
             }
-        }
-        for(int a : alphabet){
-            System.out.print(Character.toString(a));
-        }
-        System.out.println();
-        for(int i =0; i<s.length(); i++){
-            int spell = alphabet[ s.charAt(i)-'a'];
-            if(spell<='z'){
-                answer += Character.toString(spell);
-            }else {
-                answer += Character.toString(spell - 26);
-            }
+            answer +=spell;
         }
         System.out.println(answer);
         return answer;
