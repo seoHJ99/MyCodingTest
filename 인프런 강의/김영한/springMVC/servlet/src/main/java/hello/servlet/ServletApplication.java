b101dc2d943061f0e6cc1334e44f2b4c64377d2a
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-12-07 13:35:33 +0900

    Feat: hello 서브릿 분량
    인텔리제이에서 프로젝트 열때 바로 servlet 을 열지 않고 그 상위 폴더를 열면 welcome 페이지를 못찾음

--- 인프런 강의/김영한/springMVC/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ 인프런 강의/김영한/springMVC/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 실행시 현재 패키지 + 하위 패키지를 뒤져서 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
