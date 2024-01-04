6dfe43a50f60f1d919b4fccd3f101e6d2fdc5eaf
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-04 14:43:36 +0900

    Feat: 추가. 속성

--- 인프런 강의/김영한/springMVC2/thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
+++ 인프런 강의/김영한/springMVC2/thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
@@ -96,7 +96,12 @@ public class BasicController {
         return "basic/operation";
     }
 
-    @GetMapping("attribute")
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+
 
     @Data
     static class User{
