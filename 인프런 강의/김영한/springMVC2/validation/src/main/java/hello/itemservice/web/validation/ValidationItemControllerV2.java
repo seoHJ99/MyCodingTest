f81e6d59e3384ab9a62715f2dd6a9d827e520bfb
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-10 19:42:37 +0900

    Feat: 추가. FieldError, ObjectError

--- 인프런 강의/김영한/springMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ 인프런 강의/김영한/springMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,7 +45,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     // BindingResult 는 어떤 객체를 바인딩하는지 알기 위해서 @ModelAttribute 다음에 외야만 한다.
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
 
@@ -60,6 +60,43 @@ public class ValidationItemControllerV2 {
         }
         if(item.getQuantity()== null || item.getQuantity()>=9999){
             bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice<10000){
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            // bindingFailure : 바인딩 자체를 실패했는지 묻는 것
+            bindingResult.addError(new FieldError("item", "itemName",item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice()> 1_000_000){
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1000 ~ 1,000,000까지 허용합니다."));
+
+        }
+        if(item.getQuantity()== null || item.getQuantity()>=9999){
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999까지 허용합니다."));
         }
 
         // 특정 필드가 아닌 복합 룰 검증
