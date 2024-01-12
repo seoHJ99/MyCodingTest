be44c3c477f7623c7b6e981c857c7806fb1e8045
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-12 15:02:34 +0900

    Feat: 추가. Form 전송 객체 분리 - 개발

--- 인프런 강의/김영한/springMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 인프런 강의/김영한/springMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script="_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max = 1000000)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min=1000, max = 1000000)
     private Integer price;
 
-    @NotNull
-    @Max(value = 9999, groups = {SaveCheck.class}) // 저장 요구사항
+//    @NotNull
+//    @Max(value = 9999, groups = {SaveCheck.class}) // 저장 요구사항
     private Integer quantity;
 
     public Item() {
