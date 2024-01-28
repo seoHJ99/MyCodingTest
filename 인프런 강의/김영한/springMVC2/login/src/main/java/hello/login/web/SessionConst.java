a4e764bce7aefcdecaa344bb79a9044c4ba494e6
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-01-28 16:13:22 +0900

    Feat: 추가. 로그인 처리하기 - 서블릿 HTTP 세션1

---
+++ 인프런 강의/김영한/springMVC2/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+public abstract class SessionConst { // static 변수들을 담기 위해 만든 클래스. 객체를 생성하지 못하게 추상 클래스나 인터페이스로 설정
+    public static final String LOGIN_MEMBER="loginMember";
+
+}
