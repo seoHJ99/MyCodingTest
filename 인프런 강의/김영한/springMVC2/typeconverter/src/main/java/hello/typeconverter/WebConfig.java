9f99e8ae2fb667e6ca3c68b3b2eda3049930ee94
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-02-01 15:28:35 +0900

    Feat: 추가. 포멧터 적용하기

--- 인프런 강의/김영한/springMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ 인프런 강의/김영한/springMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 우선 순위 때문에 주석처리. MyNumberFormatter 문자 <-> 숫자 변환이나, 컨버터가 포멧터보다 우선순위가 높음.
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        // 포멧터 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
