9d0f4d2c795c4f0589c6ba8bd9677e45030e6b24
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-17 23:11:14 +0900

    Feat: 실패 - 70점.
    숙제하고 시간이 오래 남으면 대기열 숙제 하도록 수정.
    이전보다 5개 정도 더 맞음

--- 코딩테스트/LV2_DoHomework/src/Solution.java
+++ 코딩테스트/LV2_DoHomework/src/Solution.java
@@ -4,59 +4,82 @@ import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
 
-class Solution implements Comparator{
+class Solution implements Comparator {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         String[][] plans = {{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}};
         solution.solution(plans);
     }
+
     public String[] solution(String[][] plans) {
         List<String> answer = new ArrayList<>();
         List<List<String>> planList = new ArrayList<>(Arrays.stream(plans).map(element -> Arrays.stream(element).collect(Collectors.toList())).collect(Collectors.toList()));
-        Stack<String> stop = new Stack<>();
+        Stack<List> stop = new Stack<>();
         planList.sort(new Solution());
-        for (int i =0; i<planList.size(); i++) {
-            if(i<planList.size()-1){
+        for (int i = 0; i < planList.size(); i++) {
+            if (i < planList.size() - 1) {
                 LocalTime nowWorkStartTime = LocalTime.parse(planList.get(i).get(1));
-                LocalTime nextWorkStartTime = LocalTime.parse(planList.get(i+1).get(1));
-                Duration betweenTime = Duration.between( nowWorkStartTime, nextWorkStartTime );
+                LocalTime nextWorkStartTime = LocalTime.parse(planList.get(i + 1).get(1));
+                Duration betweenTime = Duration.between(nowWorkStartTime, nextWorkStartTime);
                 Duration workingTime = Duration.of(Long.parseLong(planList.get(i).get(2)), ChronoUnit.MINUTES);
-                System.out.println(planList.get(i).get(0));
-                System.out.println(betweenTime);
-                System.out.println(workingTime);
-                int result = betweenTime.compareTo(workingTime);
-                System.out.println(result);
-                System.out.println("---------");
+                int canDoWork = betweenTime.compareTo(workingTime);
 
-                if(result<0){
-                    stop.push(planList.get(i).get(0));
-                }else {
+                if (canDoWork < 0) {
+                    List work = new ArrayList<>();
+                    work.add(planList.get(i).get(0));
+                    work.add(workingTime.minus(betweenTime));
+                    stop.push(work);
+                } else if (canDoWork == 0) {
                     answer.add(planList.get(i).get(0));
+                } else {
+                    boolean stopSign = false;
+                    Duration leftBetweenTime = betweenTime.minus(workingTime);
+                    answer.add(planList.get(i).get(0));
+                    while (stopSign == false) {
+                        if(!stop.isEmpty()) {
+                            List stopWorking = stop.pop();
+                            Duration leftWorkingTime = (Duration) stopWorking.get(1);
+                            Duration leftTime = leftWorkingTime.minus(leftBetweenTime);
+                            if (!leftTime.isNegative()) {
+                                stopWorking.set(1, leftTime.abs());
+                                stop.push(stopWorking);
+                                stopSign = true;
+                            } else {
+                                answer.add(stopWorking.get(0).toString());
+                            }
+                        } else if(stop.isEmpty()){
+                            stopSign = true;
+                        }
+                    }
                 }
-            }else {
+            } else {
                 answer.add(planList.get(i).get(0));
+                while (true){
+                    if(stop.isEmpty()){
+                        break;
+                    }
+                    List pop = stop.pop();
+                    answer.add((String) pop.get(0));
+                }
             }
         }
-        while (!stop.isEmpty()){
-            answer.add(stop.pop());
+
+        for (String list : answer) {
+            System.out.println(list);
         }
 
-        for (String s : answer) {
-            System.out.println(s);
-        }
         String arr[] = answer.toArray(new String[answer.size()]);
-
         return arr;
     }
 
     @Override
     public int compare(Object o1, Object o2) {
-        List<String> list1 = (List<String>)o1;
-        List<String> list2 = (List<String>)o2;
-        if(LocalTime.parse(list1.get(1)).isBefore(LocalTime.parse(list2.get(1)))){
+        List<String> list1 = (List<String>) o1;
+        List<String> list2 = (List<String>) o2;
+        if (LocalTime.parse(list1.get(1)).isBefore(LocalTime.parse(list2.get(1)))) {
             return -1;
-        }else {
+        } else {
             return 1;
         }
     }
