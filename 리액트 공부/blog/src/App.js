9664619ce5b1236f17f641948f16b72c6b2964f5
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-20 19:12:58 +0900

    Feat: 추가. 리엑트에서 레이아웃 만들기
    jsx의 세가지 문법
    1. style 넣을때는 {}내부에 다시 {}. -대신 대문자로
    2. class는 className 으로 표기
    3. 중괄호를 통해서 변수를 넣을수 있음

--- 리액트 공부/blog/src/App.js
+++ 리액트 공부/blog/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let post = "강남 강남";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: "red" }}>블로그</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
