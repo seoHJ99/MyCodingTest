da6c446e585f37e408eeb77a25ca9fb66f879aac
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-20 19:40:44 +0900

    Feat: 추가. 버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- 리액트 공부/blog/src/App.js
+++ 리액트 공부/blog/src/App.js
@@ -1,17 +1,13 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   let post = "강남 강남";
-  let [글제목, b] = useState("남자 코트 추천");
-  // a : state 에 담긴 데이터를 나타냄
-  // b : state 를 다루는 함수
-
-  // state 를 쓰는 이유
-  // 변수에 담긴 데이터가 바뀔 경우, html 이 다시 렌더링되지 않아서 기존에 데이터 그대로 나옴
-  // 반면 state 는 변경되면 state를 사용하던 html 이 자동 렌더링됨
-  // 자주 변경되는 것, 변경 사항이 바로 반영되는 것을 state에 담음
+  let [글제목, b] = useState(["남자 코트 추천", "글제목 2", "글제목 3"]);
+  let [좋아요, 좋아요변경] = useState(0);
 
   return (
     <div className="App">
@@ -19,7 +15,25 @@ function App() {
         <h4>블로그</h4>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              좋아요변경(++좋아요);
+            }}
+          >
+            😊
+          </span>{" "}
+          {좋아요}
+        </h4>
+        <p>발행 날짜</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>발행 날짜</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>발행 날짜</p>
       </div>
     </div>
