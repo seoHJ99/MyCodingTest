9bf40ababc5ac69556252076b8fa2fc86dd3633d
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-21 12:20:19 +0900

    Feat: 추가. 예외 포함과 스택 트레이스

--- 인프런 강의/김영한/springDB1/jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ 인프런 강의/김영한/springDB1/jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -3,7 +3,6 @@ package hello.jdbc.exception.basic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.net.ConnectException;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -16,6 +15,17 @@ public class UncheckedAppTest {
         Controller controller = new Controller();
         assertThatThrownBy(()->controller.request())
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        }catch (Exception e){
+//            e.printStackTrace();
+            log.info("ex",e);
+        }
     }
 
     static class Controller{
