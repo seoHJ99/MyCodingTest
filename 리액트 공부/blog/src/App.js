28cf809d4a97b490df8272cb73fbc3626ab0c355
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-21 15:42:43 +0900

    Feat: 추가. 자식이 부모의 state 가져다쓰고 싶을 때는 props

--- 리액트 공부/blog/src/App.js
+++ 리액트 공부/blog/src/App.js
@@ -80,7 +80,11 @@ function App() {
         글제목.map(function (a, i) {
           return (
             <div className="list" key={i}>
-              <h4>
+              <h4
+                onClick={() => {
+                  setModal(!modal);
+                }}
+              >
                 {a}
                 <span
                   onClick={() => {
@@ -100,17 +104,26 @@ function App() {
         })
       }
 
-      {modal ? <Modal></Modal> : null}
+      {modal ? <Modal 글제목={글제목} 글제목변경={글제목변경}></Modal> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.글제목];
+          copy[0] = "여자 코트 추천";
+          props.글제목변경(copy);
+        }}
+      >
+        글 수정
+      </button>
     </div>
   );
 }
