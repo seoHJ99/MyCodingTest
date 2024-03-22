360fd70d4d125941011ad4f9ad3a760245841b37
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-03-22 11:12:53 +0900

    Feat: 추가. 스프링 예외 추상화 적용

--- 인프런 강의/김영한/springDB1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ 인프런 강의/김영한/springDB1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -48,8 +49,8 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepositoryV4_1 memberRepositoryV4(){
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepositoryV4_2 memberRepositoryV4(){
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
