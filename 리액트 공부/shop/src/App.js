411600baec44587b257f27b9517144520af0fe15
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-03 11:02:18 +0900

    Feat: 추가. 리액트 라우터 1 : 셋팅이랑 기본 라우팅

--- 리액트 공부/shop/src/App.js
+++ 리액트 공부/shop/src/App.js
@@ -12,6 +12,8 @@ import {
 import image from "./img/bg.png";
 import { useState } from "react";
 import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./detailCom.js";
 
 function App() {
   let [shoes] = useState(data);
@@ -24,25 +26,35 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
+              <Link to="/">홈</Link>
+              <Link to="/detail">상세페이지</Link>
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">장바구니</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + image + ")" }}
-      ></div> */}
-      <div className="main-bg"></div>
-      <Container>
-        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-        <Row>
-          {shoes.map(function (element, index) {
-            return <Product shoes={shoes[index]}></Product>;
-          })}
-        </Row>
-      </Container>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map(function (element, index) {
+                    return (
+                      <Product shoes={shoes[index]} i={index + 1}></Product>
+                    );
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail></Detail>} />
+      </Routes>
     </div>
   );
 }
@@ -51,7 +63,7 @@ function Product(props) {
   return (
     <Col xs={6} md={4}>
       <img
-        src="https://codingapple1.github.io/shop/shoes3.jpg"
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
         alt=""
         width="80%"
       />
