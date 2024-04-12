43890c61c341c2864a381eb81301abe73475e9e3
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 14:15:20 +0900

    Feat: 추가. 최상위 레벨 상수 const
    const 예약어를 붙이면 가장 먼저 컴파일 됨.

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,14 +1,14 @@
 
-fun main(){
+const val num = 20
+
+fun main() {
     var i = 10
-    var i2 : Int = 10
+    var i2: Int = 10
 
     var name = "호준"
-    var name2 :String = "호준"
+    var name2: String = "호준"
 
     var point = 3.3
-    var point2 : Double = 3.3
+    var point2: Double = 3.3
 
-    val num =20
-    
 }
