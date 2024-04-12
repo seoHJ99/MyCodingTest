0043c9b65177860a98783646cfafea60d7159ed6
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:01:02 +0900

    Feat: 추가. array 배열

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,12 +1,8 @@
 fun main() {
-    // 변경이 불가능한 리스트 listOf
-    val items = listOf(1, 2, 3, 4, 5)
+    val items = arrayOf(1,2,3)
+    // 0번째에 10 추가
+    items.set(0, 10)
+    items[0] = 10
 
-    // 변경이 가능한 리스트 mutableListOf
-    // 제네릭이 생략되어 있음.
-    val  items2 = mutableListOf(1, 2, 3, 4, 5)
-
-    items2.add(6)
-    items2.remove(3)
-
+    
 }
