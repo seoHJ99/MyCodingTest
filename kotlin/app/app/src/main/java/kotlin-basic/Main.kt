10e0f151e1aabca8d6f8d8c2cb08acd483a6554e
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:41:45 +0900

    Feat: 추가. 인터페이스

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,15 +1,23 @@
-abstract class Animal{  // abstract로 추상클래스로 만드는 대신 open 도 사용 가능.
-    // 자바는 기본적으로 상속받은 함수를 오버라이딩 하는 둥의 모든 행동이 허용되지만,
-    // 코틀린은 반대로 모두 막혀있기에 따로 이를 허용해 줘야만 한다.
-    // open : 자식에서 접근 가능하게 하는 키워드
+interface Drawable{
+    fun draw()
+}
+
+abstract class Animal{
     open fun move(){
         println("이동")
     }
 }
 
-class Dog : Animal(){
+// 인터페이스는 상속받을때 () 필요 없음
+// 콤마(,) 를 통해 다중 상속 가능
+
+class Dog : Animal(), Drawable {
     override fun move() {
         println("껑충")
+    }
+
+    override fun draw() {
+        TODO("Not yet implemented")
     }
 }
 
