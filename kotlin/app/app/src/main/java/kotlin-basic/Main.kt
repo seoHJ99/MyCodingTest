6aebc31fabdb3f44857764a01b8bee01d61b3c36
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 14:22:59 +0900

    Feat: 추가. max, min
    자바의 Math 클래스 내부 max, min 과 동일.
    코들린으로 작성된 함수여서 조금더 성능이 좋음

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,11 +1,9 @@
+import kotlin.math.max
 
 
 fun main() {
-    var hello = "hello"
-    println(hello.uppercase())
-    println(hello[0])
+    var i = 10
+    var j = 20
 
-    var name = "호준"
-    println("제이름은 ${name + 10}입니다.")
-    
+    println(max(i,j))
 }
