07fff1d2fd86abd5b6a34392fc3630e59468b75b
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:29:13 +0900

    Feat: 추가. 클래스간의 비교.
    data 키워드를 이용한 비교 가능.
    
    생성자 함수를 이용하지 않는 내부 필드도 외부에서 접근 가능

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,14 +1,43 @@
 fun main() {
     var john = Person("John", 20)
-//    println(john.name)
-    print(john.age)
+    var john2 = Person("John", 20)
+
+    println(john)
+    println(john2)
+    print(john == john2) // true
+
+    // 외부에서도 클래스 내부 필드 접근 가능
+    john.hobby = "야구"
 
 }
 
-class Person(
-    // getter 도 제공하지 않기 위한 키워드 private
-    // 클래스 외부에서 아예 접근 불가능.
-    // 안적으면 기본적으로 public이 기본값
-    private val name: String,
+// equal 과 hashcod 함수를 재정의하지 않고도 같음을 비교하기 위한 키워드 data
+data class Person(
+    val name: String,
     var age: Int
-)
+){
+    var hobby = "축구"
+
+    init{
+        println("생성됨")
+    }
+    
+    // 아래 코드를 사용하지 않고도 구현 가능
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//
+//        other as Person
+//
+//        if (name != other.name) return false
+//        if (age != other.age) return false
+//
+//        return true
+//    }
+//
+//    override fun hashCode(): Int {
+//        var result = name.hashCode()
+//        result = 31 * result + age
+//        return result
+//    }
+}
