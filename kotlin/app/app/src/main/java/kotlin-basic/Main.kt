8558e33985542184c66cda7bbe6ac246556c42cb
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 14:12:45 +0900

    Feat: 추가. 코틀린의 변수
    변수를 선언할때는 var을 씀.
    기본적으로 타입 추론.
    
    그러나 : 타입 을 통해 명시적으로 지정 가능.

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,4 +1,11 @@
 
 fun main(){
-    println("Hello world")
+    var i = 10
+    var i2 : Int = 10
+
+    var name = "호준"
+    var name2 :String = "호준"
+
+    var point = 3.3
+    var point2 : Double = 3.3
 }
