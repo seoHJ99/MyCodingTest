4080413fb0b18617812f8dc861738b0601713567
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:32:25 +0900

    Feat: 추가. 클래스 getter/setter 제어
    private 키워드를 통해 사용 불가능하게 만듬.

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,43 +1,20 @@
 fun main() {
     var john = Person("John", 20)
-    var john2 = Person("John", 20)
 
-    println(john)
-    println(john2)
-    print(john == john2) // true
-
-    // 외부에서도 클래스 내부 필드 접근 가능
-    john.hobby = "야구"
+    println(john.hobby)
 
 }
 
-// equal 과 hashcod 함수를 재정의하지 않고도 같음을 비교하기 위한 키워드 data
-data class Person(
+class Person(
     val name: String,
     var age: Int
 ){
     var hobby = "축구"
+        private set // set 함수 접근 불가능하게 만듬.
+        get() = "취미: $field"  // getter 함수의 재정의. return 값에 문자열 넣음.
 
     init{
         println("생성됨")
     }
-    
-    // 아래 코드를 사용하지 않고도 구현 가능
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) return true
-//        if (javaClass != other?.javaClass) return false
-//
-//        other as Person
-//
-//        if (name != other.name) return false
-//        if (age != other.age) return false
-//
-//        return true
-//    }
-//
-//    override fun hashCode(): Int {
-//        var result = name.hashCode()
-//        result = 31 * result + age
-//        return result
-//    }
+
 }
