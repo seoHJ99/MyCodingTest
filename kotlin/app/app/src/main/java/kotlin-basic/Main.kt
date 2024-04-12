3507930e85e8906a685e11b5c6f0e6e67f9c72b0
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:38:54 +0900

    Feat: 추가. 상속 extends
    open 키워드를 사용해야 상속, 오버라이딩 가능

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,20 +1,17 @@
-fun main() {
-    var john = Person("John", 20)
-
-    println(john.hobby)
-
-}
-
-class Person(
-    val name: String,
-    var age: Int
-){
-    var hobby = "축구"
-        private set // set 함수 접근 불가능하게 만듬.
-        get() = "취미: $field"  // getter 함수의 재정의. return 값에 문자열 넣음.
-
-    init{
-        println("생성됨")
+abstract class Animal{  // abstract로 추상클래스로 만드는 대신 open 도 사용 가능.
+    // 자바는 기본적으로 상속받은 함수를 오버라이딩 하는 둥의 모든 행동이 허용되지만,
+    // 코틀린은 반대로 모두 막혀있기에 따로 이를 허용해 줘야만 한다.
+    // open : 자식에서 접근 가능하게 하는 키워드
+    open fun move(){
+        println("이동")
     }
-
 }
+
+class Dog : Animal(){
+    override fun move() {
+        println("껑충")
+    }
+}
+
+class Cat : Animal()
+
