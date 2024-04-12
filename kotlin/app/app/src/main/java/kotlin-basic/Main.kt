0cbe4e9e006e392b8d048d4d4b68a61c40a40537
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:19:59 +0900

    Feat: 추가. private 키워드

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,18 +1,14 @@
 fun main() {
-    // new 키워드를 안쓰고도 객체 생성 가능
     var john = Person("John", 20)
-    println(john.name)
+//    println(john.name)
     print(john.age)
 
-    // val는 변경 불가능
-    // 이것을 이용해서 getter/setter 만들면 됨
-//    john.name = "ss"
-    john.age = 23
 }
 
-// 클래스의 내부 필드는 괄호 안에 적음
-// 길어질수 있기에 줄바꿈도 지원.
 class Person(
-    val name: String,
+    // getter 도 제공하지 않기 위한 키워드 private
+    // 클래스 외부에서 아예 접근 불가능.
+    // 안적으면 기본적으로 public이 기본값
+    private val name: String,
     var age: Int
 )
