2aab786d13da0eea255fd0fbe0e54e78afe8cfd1
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-12 15:02:40 +0900

    Feat: 추가. try - catch 오류 잡기

--- kotlin/app/app/src/main/java/kotlin-basic/Main.kt
+++ kotlin/app/app/src/main/java/kotlin-basic/Main.kt
@@ -1,8 +1,14 @@
+import java.lang.Exception
+
 fun main() {
     val items = arrayOf(1,2,3)
     // 0번째에 10 추가
     items.set(0, 10)
     items[0] = 10
 
-    
+    try {
+        val item = items[3]
+    }catch (e:Exception){
+        println(e.message)
+    }
 }
