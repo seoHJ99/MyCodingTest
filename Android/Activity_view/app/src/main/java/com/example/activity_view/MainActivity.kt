5d4f25c27dafeedfe45475a4226798b48a6a7573
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-16 06:28:33 +0900

    Feat: 추가. view 를 이용한 화면 구성

--- Android/Activity_view/app/src/main/java/com/example/activity_view/MainActivity.kt
+++ Android/Activity_view/app/src/main/java/com/example/activity_view/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.activity_view
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
