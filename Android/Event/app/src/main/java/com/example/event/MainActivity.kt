b442668272660897b5d548ec63f7541dd9fb56e2
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-21 17:56:14 +0900

    Feat: 추가. 클릭 이벤트. sam

--- Android/Event/app/src/main/java/com/example/event/MainActivity.kt
+++ Android/Event/app/src/main/java/com/example/event/MainActivity.kt
@@ -22,6 +22,18 @@ class MainActivity : AppCompatActivity() {
                 Log.d("MOTION", "체크 변경됨")
             }
         })
+
+        // SAM(Single Abstract Method) 기법. 추상 메서드가 하나일때 클래스 객체를 안만들고 바로 재정의 가능
+        binding.button.setOnClickListener{
+            Log.d("MOTION", "짧게 클릭")
+        }
+
+        binding.button.setOnLongClickListener{
+            Log.d("MOTION", "길게 클릭")
+            // 리턴값이 true이면 LongClick리스너 함수만 실행. 클릭x
+            // false면 둘다 실행
+            true
+        }
     }
     
 }
