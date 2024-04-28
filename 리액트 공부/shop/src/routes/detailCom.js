965f2f9445733384839c51cf6a4f2d8d15d94812
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-28 15:09:06 +0900

    Feat: 추가. styled-components 쓰면 CSS 파일 없어도 되는데

--- 리액트 공부/shop/src/routes/detailCom.js
+++ 리액트 공부/shop/src/routes/detailCom.js
@@ -1,4 +1,22 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// styled.태그 스타일 css 적용하면 페이지 로딩 시간 단축. 현재 페이지에만 적용 가능
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+// 기존 css 복사해서 커스텀 가능
+let newBtn = styled.button(YellowBtn);
+
+let Box = styled.div`
+  background : Grey;
+  padding 20px
+`;
+// css 파일 이름을 js파일이름.modules.css 형식으로 작성하면 해당 모듈에만 css적용됨
 
 function Detail(props) {
   let { id } = useParams();
@@ -9,6 +27,11 @@ function Detail(props) {
   console.log(findProduct);
   return (
     <div className="container">
+      <Box>
+        <YellowBtn bg="blue">버튼</YellowBtn>
+        <YellowBtn bg="orange">버튼</YellowBtn>
+      </Box>
+
       <div className="row">
         <div className="col-md-6">
           <img
