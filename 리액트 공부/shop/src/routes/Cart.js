6e5146a066e61ae9c178f79686ec6586ab99f1da
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-05-02 16:26:55 +0900

    Feat: 추가. Redux 2 : store에 state 보관하고 쓰는 법

--- 리액트 공부/shop/src/routes/Cart.js
+++ 리액트 공부/shop/src/routes/Cart.js
@@ -1,6 +1,10 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  let a = useSelector((state) => state.info);
+  console.log(a);
+
   return (
     <div>
       <Table>
@@ -13,12 +17,16 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {a.map(function (element, index) {
+            return (
+              <tr>
+                <td>{element.id}</td>
+                <td>{element.name}</td>
+                <td>{element.count}</td>
+                <td>안녕</td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
--- 리액트 공부/shop/src/store.js
+++ 리액트 공부/shop/src/store.js
@@ -1,5 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// useState 와 비슷한 역할
+let user = createSlice({
+  name: "user", // state이름
+  initialState: "kim", // 값
+});
+
+let info = createSlice({
+  name: "info",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
+
+let stock = createSlice({
+  name: "stock", // state이름
+  initialState: [10, 11, 12], // 값
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer,
+    stock: stock.reducer,
+    info: info.reducer,
+  }, // 등록
 });
