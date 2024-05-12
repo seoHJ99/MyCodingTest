9f26fcff02efd645516da2e36105b99095dc9904
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-05-12 09:58:46 +0900

    Feat: 정답 참조.
    접근 방식이 아예 달랐음.
    1. 미사일 꼬리 위치 기준으로 정렬
    2. 요격기 0에 배치
    3. 미사일을 순회하면서 해당 미사일 머리가 요격기 뒤에 있는지 검사
    4. 요격기 뒤에 있다면 요격. 즉, 요격기 위치를 해당 미사일 꼬리 위치로 이동.
    이 덕분에 겹치는 미사일들도 같이 요격됨
    5. 계속 반복

--- 코딩테스트/Lv2_InterceptSystem/src/Solution.java
+++ 코딩테스트/Lv2_InterceptSystem/src/Solution.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 class Solution {
 
@@ -9,81 +8,29 @@ class Solution {
         solution.solution(targets);
     }
 
-
-    // 무조건 모든 미사일을 통과해야 함
-    // 먼저 가장 짧은 미사일의 범위를 통과 범위로 포함시킴
-    // 그리고 그 중 가장 많은 교점이 생기는 부분을 통과?
-    // 통과된 미사일들은 모두 제외
-    // 그리고 그 다음으로 짧은 미사일로 넘어가는 식으로 구현
-
-    /*
-    1. for문을 돌면서 모든 미사일 순회
-    2. 가장 짧은 미사일 구함
-    3. 가장 짧은 미사일과 같은 x축 선상에 존재하는 미사일들 구함. 이상의 미사일들은 격추된 것으로 간주
-    4. 격추된 미사일들의 인덱스 번호는 destroyed 에 저장.
-    5. 다시 1로 돌아감. destroyed 가 다 찰때까지 반복
-     */
-
+    // 풀이 정답 참조
+    // 미사일 끝 위치를 기준으로 정렬
+    // 미사일 뒤 꽁무니만 부수면 됨
+    // 처음 요격기는 0에 위치
+    // 만약 미사일 머리 위치가 요격기보다 뒤에 있다면, 즉, 파괴되지 않았다면 요격기가 요격
+    // 요격은 미사일 꽁무니를 쏘는 방식으로 진행
+    // 따라서 미사일 뒷 꽁무니로 요격기 배치. 요격 횟수 1회 증가.
+    // 미사일 머리 위치 검사부터 미사일 횟수 반복
+    // 겹치는 미사일을 중복으로 검사하지 않는 이유는 미사일 머리 위치를 검사하여 요격되지 않은 미사일만 요격하기 때문
     public int solution(int[][] targets) {
         int answer = 0;
-        // 1. 짧은 순으로 미사일을 구함
-        // 2. 해당 미사일이 파괴되었으면 건너뜀
-        // 3. 아니면 해당 미사일과 겹치는 선상에 존재하는 미사일 모두 파괴
-        // 4. 반복
 
-        targets = sortAccordingToDistance(targets);
+        Arrays.sort(targets, ((o1, o2) -> o1[1] - o2[1]));
 
-        boolean[] destroyed = new boolean[targets.length];
-        for(int i =0; i<targets.length; i++){
-            if(destroyed[i] == true){
-                continue;
+        int destroyer = 0;
+
+        for (int i = 0; i < targets.length; i++) {
+            if(targets[i][0] >= destroyer){
+                destroyer = targets[i][1];
+                answer++;
             }
-            int [] shortest = targets[i];
-            for(int j =0; j< targets.length; j++){
-
-                if(destroyed[j]){
-                    continue;
-                }
-
-                int start = shortest[0];
-                int end = shortest[1];
-                if(targets[j][0] <=start){
-                    if(targets[j][1] >=end){
-                        destroyed[j] = true;
-                    } else if (targets[j][1] <end && targets[j][1] >start) {
-                        destroyed[j] = true;
-                    }
-                }
-            }
-            answer++;
         }
+
         return answer;
     }
-
-
-
-    public int[][] sortAccordingToDistance(int[][] targets){
-        int distance[] = new int[targets.length];
-        for(int i=0; i<targets.length; i++){
-            distance[i] = targets[i][1] - targets[i][0];
-        }
-
-        for(int i =0; i<targets.length; i++){
-            for(int j =i+1; j<targets.length; j++){
-                if(distance[i] > distance[j]){
-                    int temp = distance[i];
-                    int[] tempTarget = targets[i];
-                    distance[i] = distance[j];
-                    distance[j] = temp;
-                    targets[i] = targets[j];
-                    targets[j] = tempTarget;
-                }
-            }
-        }
-        for(int[] j : targets){
-            System.out.println(Arrays.toString(j));
-        }
-        return targets;
-    }
-
 }
